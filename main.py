f76870749805b8c66c05761c9ff9ce119153aef2
A-Wu5 <roboanthony@gmail.com>
2024-08-22 11:53:24 -0700

    Updated main.py to write output to output file

--- main.py
+++ main.py
@@ -7,13 +7,31 @@ if __name__ == "__main__":
         
         lookupTable = open(lookupFile, "r")
         flowLog = open(logFile, "r")
-    
         print("Files opened successfully")
-        tagMap, comboCount, tagCount = initMaps(lookupTable)
 
+        # Initialize dictionaries with the lookup table values
+        tagMap, comboCount, tagCount = initMaps(lookupTable)
+        # Update dictionaries with the flowlog data
+        parseLogs(flowLog, tagMap, comboCount, tagCount)
+
+        output = open("./output.txt", "w")
+        logOutput(output, comboCount, tagCount)
+
+        # print("------TagMap-----")
+        # for t in tagMap:
+        #     print(t, tagMap[t])
+        # print("-----comboCount-----")
+        # for c in comboCount:
+        #     print(c, comboCount[c])
+        # print("-----tagCount-----")
+        # for tc in tagCount:
+        #     print(tc, tagCount[tc])
+
+        output.close()
         lookupTable.close()
         flowLog.close()
     except Exception as e:
+        print("Error")
         print(e)
 
     
