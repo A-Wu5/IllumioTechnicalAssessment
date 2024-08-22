e0c901dd33ba881ba0f4813f771d5f283a62ebff
A-Wu5 <roboanthony@gmail.com>
2024-08-22 11:52:49 -0700

    Created parseLog function and logOutput function

--- initData.py
+++ initData.py
@@ -1,7 +1,9 @@
+from protocols import protocols
 
-def initMaps(lookupTable : str):
+def initMaps(lookupTable):
     """
-    Takes an open Lookup-table file descriptor and parses the data into three hashmaps \n
+    Takes an open Lookup-table file object and parses the data into three hashmaps 
+
     Pre-condition: Lookup Table file object (opened)
     Post-condition: Reads file object and returns a dictionaries
                     (1) Key: Tuple(dstport, protocol), Value: Tag : String
@@ -28,4 +30,46 @@ def initMaps(lookupTable : str):
         tagCount[tag] = 0
 
     return (tagMap, comboCount, tagCount)
+
+def parseLogs(flowLog, tagMap, comboCount, tagCount):
+    """
+    Updates dictionaries based on flowLog data
+
+    Pre-condition: flowLog file object, dictionary that maps key to tag, dictionary 
+    that maps key combinations to the count of the combinations, dictionary that counts
+    the frequency of the tags
+    Post-condition: tagMap, comboCount map, and tagCount map are updated based on the 
+    flowLog file
+    """
+    for line in flowLog:
+        flowData = line.strip().split()
+
+        # dstport is at index 6
+        dstport = flowData[6]
+
+        # Protocol is at index 7
+        protocolNumber = flowData[7]
+        protocol = protocols[protocolNumber]
+        key = (dstport, protocol)
+        if key in tagMap:
+            tag = tagMap[(dstport, protocol)]
+            comboCount[(dstport, protocol)] += 1
+            tagCount[tag] += 1
+        else:
+            tagCount["Untagged"] = tagCount.get("Untagged", 0) + 1
+
+def logOutput(outputFile, comboCount, tagCount):
+    """
+    Takes output file object and writes data from initialized maps to it
+    """
+    outputFile.write("Tag Counts: \n")
+    outputFile.write("Tag,Count \n")
+    for k, v in tagCount.items():
+        outputFile.write(k + "," + str(v) + "\n")
+    outputFile.write("\nPort/Protocol Combination Counts: \n")
+    outputFile.write("Port,Protocol,Count \n")
+    for k, v in comboCount.items():
+        dstPort = k[0]
+        protocol = k[1]
+        outputFile.write(dstPort + "," + protocol + "," + str(v) + "\n")
 
